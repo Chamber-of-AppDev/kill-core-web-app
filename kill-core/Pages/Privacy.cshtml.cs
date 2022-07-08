b1089a39396cdb01c3aef53e45387f1771c1ab6c
Daron Yondem <daron@yondem.com>
2022-07-08 12:36:01 +0300

    Privacy Page CPU Load

--- kill-core/Pages/Privacy.cshtml.cs
+++ kill-core/Pages/Privacy.cshtml.cs
@@ -6,6 +6,8 @@ namespace kill_core.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly List<Thread> threads = new();
+        private readonly Random rand = new();
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
@@ -14,6 +16,30 @@ namespace kill_core.Pages
 
         public void OnGet()
         {
+            DateTime dt1 = DateTime.Now;
+
+            while (true)
+            {
+                threads.Add(new Thread(new ThreadStart(KillCore)));
+                if (DateTime.Now.Subtract(dt1).TotalSeconds > 30)
+                {
+                    StopThreads();
+                    break;
+                }                    
+            }
+        }
+        public void KillCore()
+        {
+            long num = 0;
+            while (true)
+            {
+                num += int.Parse(rand.Next(10, 100).ToString());
+                if (num > 1000000) { num = 0; }
+            }
+        }
+        public void StopThreads()
+        {
+            foreach (Thread t in threads) { t.Interrupt(); }
         }
     }
 }
